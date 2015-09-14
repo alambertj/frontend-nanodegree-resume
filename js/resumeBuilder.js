44eb368d3064f9fb7c4851a0a25b5023bbf97def
AJ <alambertj@gmail.com>
2015-09-14 11:02:12 -0400

    edits
    bg change, create new JSON objects

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,60 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+var bio = {
+	"name": "Allison Jones",
+	"age": 28,
+	"role": "UI/UX Designer",
+	"bioPic": "images/fry.jpg",
+	"contacts": {
+		"email": "alambertj@gmail.com"
+	},
+	"welcomeMessage": "hi",	
+	"skills": skills
+}
+
+var work = {
+	"position": "UI/UX Designer",
+	"employer": "NetBeez",
+	"years": "2013 - present",
+	"city": "Pittsburgh"	
+}
+
+var education = {
+	"schools": [
+		{
+			"name": "Carnegie Mellon University",
+			"years": "2005-2009",
+			"graduated": "2009",
+			"city": "Pittsburgh, PA",
+			"major": "Industrial Design"
+		},
+		{
+			"name": "The Branson School",
+			"years": "2001-2005",
+			"graduated": "2005",
+			"city": "Ross, CA"
+		}
+	],
+	"online courses": [
+	]
+}
+
+var projects = {
+	"project": [
+		{
+			"title": "Astrobotic Logo Design",
+			"dates": "2011",
+			"description": "I designed a logo",
+			"images": "[images go here]"
+		},
+		{
+			"title": "Book Cover Design",
+			"dates": "2014",
+			"description": "I designed a book cover",
+			"images": "[images go here]"
+		}
+	
+	]
+}
